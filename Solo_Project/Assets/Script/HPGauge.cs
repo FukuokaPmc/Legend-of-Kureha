35e0869126f4dbf0649970d9d2a97b3bdba5bd2c
FukuokaToshiki <fukuoka@pmc.co.jp>
2016-10-18 19:37:41 +0900

    ・HPゲージ実装。そのうちプレイヤーと同期。

---
+++ Solo_Project/Assets/Script/HPGauge.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HPGauge : MonoBehaviour {
+    private Image Gauge;
+    private Image Minus;
+    private float HP;
+	// Use this for initialization
+	void Start () {
+        HP = 1.0f;
+        Gauge = this.transform.GetChild(1).GetComponent<Image>();
+        Minus = this.transform.GetChild(0).GetComponent<Image>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.V))
+        {
+            HP -= 0.1f;
+            Gauge.fillAmount = HP;
+        }
+
+        if(Minus.fillAmount > HP)
+        {
+            Minus.fillAmount -= 0.003f;
+        }
+	}
+}
