47a6e9cc99fd98c7981d9f3f509781f464163404
FukuokaToshiki <fukuoka@pmc.co.jp>
2016-10-20 21:06:44 +0900

    ・ボス面生成 ・ボスをとりあえず出した

---
+++ Solo_Project/Assets/Script/PhaseSystem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhaseSystem : MonoBehaviour {
+    private bool Boss; //trueでボス戦
+	// Use this for initialization
+	void Start () {
+        Boss = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void BossStage()
+    {
+        Boss = true;
+        foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Stage"))
+        {
+            Destroy(obj.gameObject);
+        }
+    }
+}
--- Solo_Project/Assets/Script/StageGenerator.cs
+++ Solo_Project/Assets/Script/StageGenerator.cs
@@ -5,12 +5,18 @@ public class StageGenerator : MonoBehaviour {
     private GameObject NextRoad;
     public bool Flag;
     private BoxCollider Trigger;
-    private int Count;
+    public static int StageCount;
+    public GameObject BossStage;
+    public GameObject BossModel;
+    public GameObject BossEff;
+    private PhaseSystem phase;
+    private int StageMax;
 	// Use this for initialization
 	void Start () {
-        Count = 0;
+        //StageCount = 0;
         Flag = false;
-        //NextRoad.SetActive(false);
+        phase = GameObject.FindGameObjectWithTag("Phase").GetComponent<PhaseSystem>();
+        StageMax = 5;
     }
 	
 	// Update is called once per frame
@@ -27,10 +33,32 @@ public class StageGenerator : MonoBehaviour {
             {
                 Vector3 NextPos;
                 NextPos = this.transform.position;
-                NextPos.z += 100.0f;
-                NextRoad = Instantiate(this.gameObject);
-                NextRoad.transform.position = NextPos;
+                if (StageCount < StageMax)
+                {
+                    NextPos.z += 100.0f;
+                    NextRoad = Instantiate(this.gameObject);
+                    NextRoad.transform.position = NextPos;
+                    StageCount++;
+                }
+                else
+                {
+                    NextPos.z += 140.0f;
+                    BossStage = Instantiate(BossStage);
+                    BossStage.transform.position = NextPos;
+                    BossModel = Instantiate(BossModel);
+                    BossModel.transform.position = BossStage.transform.position;
+                    StageCount++;
+                }
+
+                if(StageCount == StageMax - 1)
+                {
+                    NextPos.z += 190.0f;
+                    NextPos.y += 5.0f;
+                    BossEff = Instantiate(BossEff);
+                    BossEff.transform.position = NextPos;
+                }
                 Flag = true;
+                
 
             }
         }
@@ -40,8 +68,13 @@ public class StageGenerator : MonoBehaviour {
     {
         if (col.gameObject.tag == "Player")
         {
+            if(StageCount > StageMax)
+            {
+                phase.BossStage();
+            }
             if (col.transform.position.z >= this.transform.position.z + 20.0f)
             Destroy(this.gameObject);
         }
+
     }
 }
